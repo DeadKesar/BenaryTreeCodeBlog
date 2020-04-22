5bc242a728f532d2ae16657740734d18540a7ef8
DeadKesar <DeadKesar@gmail.com>
2020-04-22 20:07:22 +0300

    Добавьте файлы проекта.

---
+++ BenaryTreeCodeBlog/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenaryTreeCodeBlog
+{
+    class Node<T> :IComparable 
+            where T:IComparable
+    {
+
+        public T Data { get; set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node(T data, Node<T> right, Node<T> left)
+        {
+            Data = data;
+            Right = right;
+            Left = left;
+        }
+
+        public int CompareTo(object obj)
+        {
+            
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Not correct type", nameof(obj));
+            }
+
+        }
+    }
+}
---
+++ BenaryTreeCodeBlog/Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenaryTreeCodeBlog
+{
+    class Tree <T>
+        where T: IComparable
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+
+        public void Add (T data)
+        {
+            var node = new Node<T>(data);
+            if (Root == null)
+            {
+                Root = node;
+                Count = 1;
+                return;
+            }
+            var current = Root;
+
+            switch (current.Data.CompareTo(node.Data))
+                case (-1):
+                {
+                    
+                }
+
+        }
+
+    }
+}
